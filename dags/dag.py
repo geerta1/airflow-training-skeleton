ae15200d1f490a87c179c6a998895de4b2f46dd8
geert <geert.adriaans@knab.nl>
2018-09-18 14:33:04 +0200

    added import postgres_to_gcs operator

--- dags/dag.py
+++ dags/dag.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 
 dag = DAG(
     dag_id="my_first_dag",
